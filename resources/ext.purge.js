bb0cc0e62fee8d9af1a649db7a7937737d915c7c
Lens0021 <lorentz0021@gmail.com>
2022-02-18 05:14:08 +0900

    Use document.querySelector instead of jquery
    For some reason, preventDefault() does not work for <a> element for me. Even if it's not true, removing jquery will improve the performance.

--- resources/ext.purge.js
+++ resources/ext.purge.js
@@ -1,7 +1,7 @@
 
 mw.loader.using( [ 'mediawiki.api', 'mediawiki.notify' ] ).then( function () {
 
-	$( "#ca-purge a" ).on( 'click', function ( e ) {
+	document.querySelector( '#ca-purge a' ).addEventListener( 'click', function ( e ) {
 		var postArgs = { action: 'purge', titles: mw.config.get( 'wgPageName' ) };
 		new mw.Api().post( postArgs ).then( function () {
 			location.reload();
